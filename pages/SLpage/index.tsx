ee45236de1ab703b859ee471d2a92fe82d25b7db
A R, Anoop <anoop.ar@legatohealth.com>
2021-09-22 09:36:58 +0530

    SLpage UI enhancements, added +35% and tofixed 2

--- pages/SLpage/index.tsx
+++ pages/SLpage/index.tsx
@@ -12,12 +12,13 @@ export default () => {
       <div className="col">
         <div className="row">
             <input className="col-2" type="text" onChange={(e) => setPrice1(parseFloat(e.target.value))} />
-            {/* <p className="col">+35%</p>  */}
-          <p className="col-2 m-2">{price1 + price1 * 0.35}</p>
+            <p className="col-1 m-2">+35%</p> 
+          <h3 className="col-2 m-2 text-danger">{(price1 + price1 * 0.35).toFixed(2)}</h3>
         </div>
         <div className="row">
           <input className="col-2" type="text" onChange={(e) => setPrice2(parseFloat(e.target.value))} />
-          <p className="col-2 m-2">{price2 + price2 * 0.35}</p>
+          <p className="col-1 m-2">+35%</p> 
+          <h3 className="col-2 m-2 text-danger">{(price2 + price2 * 0.35).toFixed(2)}</h3>
         </div>
       </div>
     </div>
